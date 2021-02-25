0c32ebb2dcc46e1728bf4a6a55bc4f4666cec1e5
artemternovenko <artem.inkaz@gmail.com>
2021-02-25 12:07:17 +0600

    [ADDED] layout and model

---
+++ app/src/main/java/com/example/githubusers/models/Users.java
@@ -0,0 +1,35 @@
+package com.example.githubusers.models;
+
+import java.util.Objects;
+// final значи класс меняться не будет
+public final class Users {
+    private String name;
+    private String avatarUrl;
+
+    public Users(String name, String avatarUrl) {
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return name.equals(users.name) &&
+                avatarUrl.equals(users.avatarUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, avatarUrl);
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "name='" + name + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                '}';
+    }
+}
